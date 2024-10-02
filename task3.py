f32bb7037c71c086766f49bc06b11e2208cdb42d
Frolite <vladvalakas14@gmail.com>
2024-10-02 14:38:43 +0300

    Add files via upload

---
+++ task3.py
@@ -0,0 +1,5 @@
+made_dz = 12
+hour = 1.5
+name = "Python"
+run_time = hour / made_dz
+print('Курс:', name, ', всего задач:', str(made_dz), ', затрачено часов:', str(hour), ", среднее время выполнения", str(run_time), "часа.")
