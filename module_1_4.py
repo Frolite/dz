7cdb12c8e3cf4333be8c56222c408ce914faa368
Frolite <vladvalakas14@gmail.com>
2024-10-04 16:59:39 +0300

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,9 @@
+my_string = input()
+num = int(len(my_string))
+print(num)
+
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.replace(' ', ''))
+print(my_string[0])
+print(my_string[num-1])
