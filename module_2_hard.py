da4e591022ecde2f792dd49bd5477634680796e8
Frolite <vladvalakas14@gmail.com>
2024-10-14 14:15:37 +0300

    Add files via upload

--- module_2_hard.py
+++ module_2_hard.py
@@ -1,11 +1,11 @@
 password = []
 for num in range(3, 21):
     for i in range(1, 20):
-        for j in range(1, 20):
-            if ((i + j) == num or num % (i + j) == 0) and i != j and j > i:
+        for j in range(2, 20):
+            if num % (i + j) == 0 and i != j and j > i:
                 password.append(i)
                 password.append(j)
             else:
                 continue
-    print(f"{num} - " + ''.join(map(str, password)))
+    print(f"{num} - " + ' '.join(map(str, password)))
     password.clear()
