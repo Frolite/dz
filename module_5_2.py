af038260ba0f38a26c7851457943deddb6baf7b5
Frolite <vladvalakas14@gmail.com>
2024-12-07 10:00:21 +0300

    Add files via upload

--- module_5_2.py
+++ module_5_2.py
@@ -3,6 +3,18 @@ class House:
         self.name = name
         self.number_of_floor = number_of_floor
 
+
+    def go_to(self, new_floor: int)->int:
+        if new_floor <= self.number_of_floor:
+            i = 1
+            while i <= new_floor:
+                print(i)
+                i+=1
+
+        else:
+            print("Такого этажа не существует.")
+
+
     def __len__(self):
         return self.number_of_floor
     
@@ -14,10 +26,12 @@ class House:
 h1 = House('ЖК Эльбрус', 10)
 h2 = House('ЖК Акация', 20)
 
-# __str__
-print(h1)
+
+print(h1) # __str__
 print(h2)
 
-# __len__
-print(len(h1))
+h1.go_to(5)
+h2.go_to(10)
+
+print(len(h1)) # __len__
 print(len(h2))
