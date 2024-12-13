c32187a0dc718b2cb843e73d7ac1ae59063f0884
Frolite <vladvalakas14@gmail.com>
2024-12-13 09:06:53 +0300

    Update module_5_3.py

--- module_5_3.py
+++ module_5_3.py
@@ -1,116 +1,89 @@
 class House:
-    def __init__(self, name, number_of_floor):
-        self.name = name
-        self.number_of_floor = number_of_floor
-
-
-    def go_to(self, new_floor: int)->int:
-        if new_floor <= self.number_of_floor:
-            i = 1
-            while i <= new_floor:
-                print(i)
-                i+=1
-
-        else:
-            print("Такого этажа не существует.")
-
-
-    def __len__(self):
-        return self.number_of_floor
-    
+    def __init__(self, name, floors):
+        self.name = name  # Название дома
+        self.floors = floors  # Количество этажей
 
     def __str__(self):
-        return f"Название: {self.name}, кол-во этажей: {self.number_of_floor}"
-
+        return f"Название: {self.name}, кол-во этажей: {self.floors}"
 
     def __eq__(self, other):
-        if isinstance(other, int) == True and isinstance(other, House) == True:
-            return self.number_of_floor == other.number_of_floor
-        else:
-            return None
+        if isinstance(other, House):
+            return self.floors == other.floors
+        elif isinstance(other, int):
+            return self.floors == other
+        return False
 
-    
     def __lt__(self, other):
-        if isinstance(other, int) == True and isinstance(other, House) == True:
-            return self.number_of_floor < other.number_of_floor
-        else:
-            return None
+        if isinstance(other, House):
+            return self.floors < other.floors
+        elif isinstance(other, int):
+            return self.floors < other
+        return False
 
-    
     def __le__(self, other):
-        if isinstance(other, int) == True and isinstance(other, House) == True:
-            return self.number_of_floor <= other.number_of_floor
-        else:
-            return None
+        if isinstance(other, House):
+            return self.floors <= other.floors
+        elif isinstance(other, int):
+            return self.floors <= other
+        return False
 
-    
     def __gt__(self, other):
-        if isinstance(other, int) == True and isinstance(other, House) == True:
-            return self.number_of_floor > other.number_of_floor
-        else:
-            return None
+        if isinstance(other, House):
+            return self.floors > other.floors
+        elif isinstance(other, int):
+            return self.floors > other
+        return False
 
-    
     def __ge__(self, other):
-        if isinstance(other, int) == True and isinstance(other, House) == True:
-            return self.number_of_floor >= other.number_of_floor
-        else:
-            return None
-
+        if isinstance(other, House):
+            return self.floors >= other.floors
+        elif isinstance(other, int):
+            return self.floors >= other
+        return False
 
     def __ne__(self, other):
-        if isinstance(other, int) == True and isinstance(other, House) == True:
-            return self.number_of_floor != other.number_of_floor
-        else:
-            return None
-
+        if isinstance(other, House):
+            return self.floors != other.floors
+        elif isinstance(other, int):
+            return self.floors != other
+        return True
 
     def __add__(self, value):
-        if isinstance(value, int) == True:
-            return self.number_of_floor + value
-        else:
-            return None
+        if isinstance(value, int):
+            self.floors += value
+            return self
+        return NotImplemented
 
-    
     def __radd__(self, value):
-        if isinstance(value, int) == True:
-            return self.number_of_floor + value
-        else:
-            return None
+        return self.__add__(value)
 
-    
     def __iadd__(self, value):
-        if isinstance(value, int) == True:
-            return self.number_of_floor + value
-        else:
-            return None
+        if isinstance(value, int):
+            self.floors += value
+            return self
+        return NotImplemented
 
-
+# Пример выполнения программы
 h1 = House('ЖК Эльбрус', 10)
 h2 = House('ЖК Акация', 20)
 
-print(h1)
-print(h2)
+print(h1)  # Название: ЖК Эльбрус, кол-во этажей: 10
+print(h2)  # Название: ЖК Акация, кол-во этажей: 20
 
-h1.go_to(5)
-h2.go_to(10)
+print(h1 == h2)  # False
 
-print(len(h1)) # __len__
-print(len(h2)) 
+h1 = h1 + 10  # Увеличение этажей на 10
+print(h1)  # Название: ЖК Эльбрус, кол-во этажей: 20
+print(h1 == h2)  # True
 
-print(h1 == h2) # __eq__
+h1 += 10  # Увеличение этажей на 10 с помощью +=
+print(h1)  # Название: ЖК Эльбрус, кол-во этажей: 30
 
-h1 = h1 + 10 # __add__
-print(h1)
+h2 = 10 + h2  # Увеличение этажей на 10 с помощью radd
+print(h2)  # Название: ЖК Акация, кол-во этажей: 30
 
-h1 += 10 # __iadd__
-print(h1)
-
-h2 = 10 + h2 # __radd__
-print(h2)
-
-print(h1 > h2) # __gt__
-print(h1 >= h2) # __ge__
-print(h1 < h2) # __lt__
-print(h1 <= h2) # __le__
-print(h1 != h2) # __ne__
+print(h1 > h2)  # False
+print(h1 >= h2)  # True
+print(h1 < h2)  # False
+print(h1 <= h2)  # True
+print(h1 != h2)  # False
